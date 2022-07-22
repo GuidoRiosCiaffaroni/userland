9f42a22ad96b3d08737fe33ea26065087474b0d3
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-07-22 15:57:32 -0400

    2022_07_22_15_57_27

---
+++ wp-content/themes/simentor/footer.php
@@ -0,0 +1,17 @@
+<?php
+if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'footer' ) ) {
+?>
+<footer id="site-footer" class="site-footer" role="contentinfo">
+<div id="copyright">
+<?php
+echo sprintf( __( '%1$s %2$s %3$s.', 'simentor' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) );
+echo sprintf( __( ' Theme By: %1$s.', 'simentor' ), '<a href="https://mastarachef.org/">mastarachef.org</a>' );
+?>
+</div>	
+</footer>
+<?php
+}
+?>
+<?php wp_footer(); ?>
+</body>
+</html>
