62f339993a19ddbc62669bd3a6d14e24efce13be
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-07-23 14:42:41 -0400

    2022_07_23_14_42_26

--- wp-content/themes/AdminLTE/header.php
+++ wp-content/themes/AdminLTE/header.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
+
+<!-- --------------------------------------------------------------------------------------------------------------------------------------- -->
+<!--
 <html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta charset="<?php //bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <?php wp_head(); ?>
 </head>
@@ -11,11 +14,17 @@
 <header id="header" role="banner">
 <div id="branding">
 <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+-->
+
 <?php
+/*
 if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; }
 echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
 if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; }
+*/
 ?>
+
+<!--
 </div>
 <div id="site-description"<?php if ( !is_single() ) { echo ' itemprop="description"'; } ?>><?php bloginfo( 'description' ); ?></div>
 </div>
@@ -26,3 +35,4 @@ if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1
 </header>
 <div id="container">
 <main id="content" role="main">
+	-->
