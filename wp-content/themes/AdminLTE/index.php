f9da653a93226dd22b8251ae024ebe753197a1ea
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-07-23 15:41:59 -0400

    2022_07_23_15_41_58

--- wp-content/themes/AdminLTE/index.php
+++ wp-content/themes/AdminLTE/index.php
@@ -6,8 +6,18 @@ get_header();
       <div class="container-fluid">
         <!-- Info boxes -->
         <div class="row">
-  
-<?php echo get_template_directory_uri(); ?>
+
+
+
+<?php
+if ( have_posts() ) : while ( have_posts() ) : the_post();
+get_template_part( 'entry' );
+comments_template();
+endwhile; endif;
+get_template_part( 'nav', 'below' );
+
+?>
+
         </div>
         <!-- /.row -->
 
@@ -21,18 +31,3 @@ get_footer();
 ?>
 
 
-
-
-
-
-<?php
-/*
-get_header();
-
-if ( have_posts() ) : while ( have_posts() ) : the_post();
-get_template_part( 'entry' );
-comments_template();
-endwhile; endif;
-get_template_part( 'nav', 'below' );
-get_footer();
-*/
