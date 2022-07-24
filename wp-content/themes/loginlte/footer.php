002ed6337b812208ed5f3e2a9d49303e233d3339
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-07-24 16:02:27 -0400

    2022_07_24_16_02_22

---
+++ wp-content/themes/loginlte/footer.php
@@ -0,0 +1,75 @@
+
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="<?php echo get_template_directory_uri(); ?>/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="<?php echo get_template_directory_uri(); ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="<?php echo get_template_directory_uri(); ?>/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo get_template_directory_uri(); ?>/dist/js/adminlte.js"></script>
+
+<!-- PAGE PLUGINS -->
+<!-- jQuery Mapael -->
+<script src="<?php echo get_template_directory_uri(); ?>/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/plugins/raphael/raphael.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/plugins/jquery-mapael/maps/usa_states.min.js"></script>
+<!-- ChartJS -->
+<script src="<?php echo get_template_directory_uri(); ?>/plugins/chart.js/Chart.min.js"></script>
+
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo get_template_directory_uri(); ?>/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?php echo get_template_directory_uri(); ?>/dist/js/pages/dashboard2.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--
+</main>
+<?php get_sidebar(); ?>
+</div>
+<footer id="footer" role="contentinfo">
+<div id="copyright">
+&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+</div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
+-->
