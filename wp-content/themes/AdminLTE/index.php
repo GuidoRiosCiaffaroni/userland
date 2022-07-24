fd6a09fb88c7458773d06d40cb98d845a678bc82
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-07-24 19:14:38 -0400

    2022_07_24_19_14_29

--- wp-content/themes/AdminLTE/index.php
+++ wp-content/themes/AdminLTE/index.php
@@ -11,37 +11,11 @@ get_header();
 <?php
     if(is_user_logged_in())
     {
-    	echo 'logeado';
-    	echo '</br>';  
-        $theme_data = wp_get_theme();
-        echo $theme_data->get( 'Name' );        // Theme name as given in style.css
-        echo '<br>';
-        echo $theme_data->get( 'ThemeURI' );
-        echo '<br>';
-        echo $theme_data->get( 'Description' );
-        echo '<br>';
-        echo $theme_data->get( 'Author' );
-        echo '<br>';
-        echo $theme_data->get( 'AuthorURI' );
-        echo '<br>';
-        echo $theme_data->get( 'Version' );
-        echo '<br>';
-        echo $theme_data->get( 'Template' );
-        echo '<br>';
-        echo $theme_data->get( 'Status' );
-        echo '<br>';
-        echo $theme_data->get( 'Tags' );
-        echo '<br>';
-        echo $theme_data->get( 'TextDomain' );
-        echo '<br>';
-        echo $theme_data->get( 'DomainPath' );
         switch_theme('adminlte');
     }
     else
     {
-    echo 'nop';
-    echo '</br>';
-    switch_theme('noto-simple');
+    switch_theme('loginlte');
     }
 ?>
 
--- wp-content/themes/loginlte/index.php
+++ wp-content/themes/loginlte/index.php
@@ -1,71 +1,147 @@
 <?php
-get_header();
-?>
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <!-- Info boxes -->
-        <div class="row">
+  /*
+    $loginEmail = $_POST['loginEmail'];
+    $loginPassword = $_POST['loginPassword'];
 
+    wp_authenticate( $loginEmail, $loginPassword );
 
-<?php
     if(is_user_logged_in())
     {
-    	echo 'logeado';
-    	echo '</br>';  
-        $theme_data = wp_get_theme();
-        echo $theme_data->get( 'Name' );        // Theme name as given in style.css
-        echo '<br>';
-        echo $theme_data->get( 'ThemeURI' );
-        echo '<br>';
-        echo $theme_data->get( 'Description' );
-        echo '<br>';
-        echo $theme_data->get( 'Author' );
-        echo '<br>';
-        echo $theme_data->get( 'AuthorURI' );
-        echo '<br>';
-        echo $theme_data->get( 'Version' );
-        echo '<br>';
-        echo $theme_data->get( 'Template' );
-        echo '<br>';
-        echo $theme_data->get( 'Status' );
-        echo '<br>';
-        echo $theme_data->get( 'Tags' );
-        echo '<br>';
-        echo $theme_data->get( 'TextDomain' );
-        echo '<br>';
-        echo $theme_data->get( 'DomainPath' );
         switch_theme('adminlte');
     }
     else
     {
-    echo 'nop';
-    echo '</br>';
-    switch_theme('noto-simple');
+    switch_theme('loginlte');
     }
+*/
+
+
+
+
+   if($_POST['loginEmail'] && $_POST['loginPassword'])
+   {
+    $user = wp_authenticate( $_POST['loginEmail'] , $_POST['loginPassword'] );
+
+    if(is_wp_error($user)) 
+    {
+        echo $user->get_error_message();
+    } 
+    else 
+    {
+        if($user->user_status === "0") 
+        {
+            wp_logout();
+        } 
+        else 
+        {
+            if(is_user_logged_in()) 
+            {
+              is_user_logged_in();
+            } 
+            else 
+            {
+             
+            }
+        }
+    }
+}
+
+
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Log in</title>
 
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="../../index2.html"><b>Admin</b>LTE</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
 
-<?php
-if ( have_posts() ) : while ( have_posts() ) : the_post();
-get_template_part( 'entry' );
-comments_template();
-endwhile; endif;
-get_template_part( 'nav', 'below' );
-
-?>
-
+      <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+        <div class="input-group mb-3">
+          <input name="loginEmail" type="email" class="form-control" placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
         </div>
-        <!-- /.row -->
+        <div class="input-group mb-3">
+          <input name="loginPassword" type="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="remember">
+              <label for="remember">
+                Remember Me
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
 
-        <!-- /.row -->
-      </div><!--/. container-fluid -->
-    </section>
-    <!-- /.content -->
+      <!--
+      <div class="social-auth-links text-center mb-3">
+        <p>- OR -</p>
+        <a href="#" class="btn btn-block btn-primary">
+          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+        </a>
+        <a href="#" class="btn btn-block btn-danger">
+          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+        </a>
+      </div>
+      -->
 
-<?php
-get_footer();
-?>
+      <!-- /.social-auth-links -->
 
+      <!--
+      <p class="mb-1">
+        <a href="forgot-password.html">I forgot my password</a>
+      </p>
+      <p class="mb-0">
+        <a href="register.html" class="text-center">Register a new membership</a>
+      </p>
+      -->
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
 
+<!-- jQuery -->
+<script src="<?php echo get_template_directory_uri(); ?>//plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo get_template_directory_uri(); ?>//plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo get_template_directory_uri(); ?>//dist/js/adminlte.min.js"></script>
+</body>
+</html>
