75e4de3b7d512a22d9733e0e5c1b45bc7fd1f977
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-07-26 15:01:48 -0400

    2022_07_26_15_01_47

--- wp-content/themes/AdminLTE/header.php
+++ wp-content/themes/AdminLTE/header.php
@@ -38,33 +38,41 @@
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
       </li>
-    </ul>
-
+    
     <?php 
-   echo preg_replace( '#<li[^>]+>#', '<li class="nav-item">',
-    wp_nav_menu( 
+    $cadena = wp_nav_menu( 
       array( 
-        'theme_location' => 'main-menu', // Alias de navegación
-        'menu'            => '', // menú esperado
-        'container'       => '',// etiqueta del contenedor
-        'container_class' => 'navbar-nav', // valor de clase de nodo primario ul
-        'container_id'  => '',  // valor de id del nodo primario ul
-        'menu_class'   => 'navbar-nav',   // valor de clase de nodo ul
-        'menu_id'   => '',  // valor de id del nodo ul
-        'echo'  => false,// Ya sea para mostrar el menú, el valor predeterminado es verdadero
-        'fallback_cb' => 'wp_page_menu',  // Si el menú no existe, regrese al menú predeterminado, ajústelo en falso para que no regrese
-        'before' => '', // Texto antes del enlace
-        'after'  => '', // Texto después del enlace
-        'link_before'  => '',   // Antes del texto del enlace
-        'link_after'  => '',// Después del texto del enlace
-        'items_wrap'  => '<ul class="navbar-nav">%3$s</ul>',   // Cómo empacar la lista <ul id="%1$s" class="%2$s">%3$s</ul>
-        'depth' => 0,   // Profundidad del menú, predeterminado 0
-        'walker' => ''  // Walker personalizado
-      ) 
-    )
-  );
+              'theme_location' => 'main-menu', // Alias de navegación
+              'menu'            => '', // menú esperado
+              'container'       => '',// etiqueta del contenedor
+              'container_class' => 'navbar-nav', // valor de clase de nodo primario ul
+              'container_id'  => '',  // valor de id del nodo primario ul
+              'menu_class'   => 'navbar-nav',   // valor de clase de nodo ul
+              'menu_id'   => '',  // valor de id del nodo ul
+              'echo'  => false,// Ya sea para mostrar el menú, el valor predeterminado es verdadero
+              'fallback_cb' => 'wp_page_menu',  // Si el menú no existe, regrese al menú predeterminado, ajústelo en falso para que no regrese
+              'before' => '', // Texto antes del enlace
+              'after'  => '', // Texto después del enlace
+              'link_before'  => '',   // Antes del texto del enlace
+              'link_after'  => '',// Después del texto del enlace
+              'items_wrap'  => '%3$s',   // Cómo empacar la lista // <ul id="%1$s" class="%2$s">%3$s</ul> // <ul class="navbar-nav">%3$s</ul>
+              'depth' => 0,   // Profundidad del menú, predeterminado 0
+              'walker' => ''  // Walker personalizado
+      )
+    );
+
+  $patrones = array();
+  $patrones[0] = '#<li[^>]+>#';
+  $patrones[1] = '/itemprop="url"/';
+
+  $sustituciones = array();
+  $sustituciones[1] = '<li class="nav-item d-none d-sm-inline-block">';
+  $sustituciones[0] = ' class="nav-link" itemprop="url" ';
+
+  echo preg_replace($patrones,$sustituciones,$cadena);
   
     ?>
+</ul>
 
 
 
