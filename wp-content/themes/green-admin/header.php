06b0245a681a1974d22c1a675a63b7639cfb0141
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-07-28 13:09:03 -0400

    2022_07_28_13_09_03

--- wp-content/themes/green-admin/header.php
+++ wp-content/themes/green-admin/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+<html  <?php blankslate_schema_type(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
@@ -11,11 +11,13 @@
 <header id="header" role="banner">
 <div id="branding">
 <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+
 <?php
 if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; }
 echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
 if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; }
 ?>
+
 </div>
 <div id="site-description"<?php if ( !is_single() ) { echo ' itemprop="description"'; } ?>><?php bloginfo( 'description' ); ?></div>
 </div>
--- wp-content/themes/green-admin/index.php
+++ wp-content/themes/green-admin/index.php
@@ -608,25 +608,77 @@
 									<div class="breadcomb-icon">
 										<i class="notika-icon notika-edit"></i>
 									</div>
+
+
 									<div class="breadcomb-ctn">
-										
-
-                                        <h2>Wizard</h2>
-										<p>Welcome to Notika <span class="bread-ntd">Admin Template</span></p>
-
-                                        <?php
-                                        
+									    <?php                                       
                                         //get_header();
                                         if ( have_posts() ) : while ( have_posts() ) : the_post();
-                                        get_template_part( 'entry' );
+                                        //get_template_part( 'entry' );
+                                        ?>
+	
+
+                                        <!--
+                                        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                                        
+                                        <h2>Wizard</h2>
+                                        -->
+										
+                                        
+                                        <?php 
+                                        if ( is_singular() ) 
+                                        { 
+                                            echo '<h1 class="entry-title" itemprop="headline">'; 
+                                        } 
+                                        else 
+                                        { 
+                                            echo '<h2 class="entry-title">'; 
+                                        } 
+                                        ?>
+                                        
+                                        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
+                                        
+                                        <?php 
+                                        if ( is_singular() ) 
+                                        { 
+                                            echo '</h1>'; 
+                                        } 
+                                        else 
+                                        { 
+                                            echo '</h2>'; 
+                                        } 
+                                        ?>
+
+
+                                        <!--
+                                        <p>Welcome to Notika <span class="bread-ntd">Admin Template</span></p>
+                                        -->
+
+                                        <?php edit_post_link(); ?>
+
+                                        <?php if ( !is_search() ) { get_template_part( 'entry', 'meta' ); } ?>
+
+                                        <?php 
+                                        get_template_part('entry',( is_front_page() || is_home() || is_front_page() && is_home() || is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
+                                        <?php if ( is_singular() ) { get_template_part( 'entry-footer' ); } ?>
+
+                                        <!--
+                                        </article>
+                                        -->
+									</div>
+
+                                        <?php
                                         comments_template();
                                         endwhile; endif;
-                                        //get_template_part( 'nav', 'below' );
+                                        get_template_part( 'nav', 'below' );
                                         //get_footer();
                                         
                                         ?>
 
-									</div>
+
+
+
+
 								</div>
 							</div>
 							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-3">
