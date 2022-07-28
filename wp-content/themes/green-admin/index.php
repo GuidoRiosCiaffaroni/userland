6985567ab8f792baa9c651a68e0b9ad9de6e18bd
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-07-28 09:17:59 -0400

    2022_07_28_09_17_59

--- wp-content/themes/green-admin/index.php
+++ wp-content/themes/green-admin/index.php
@@ -7,7 +7,7 @@
     <title>Dashboard One | Notika - Notika Admin Template</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <?php wp_head(); ?>
     <!-- favicon
 		============================================ -->
     <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
