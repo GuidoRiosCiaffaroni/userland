3c2fdc61f66b4e83aa17517b3b0d7f07b6f771a7
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-08-01 08:58:44 -0400

    2022_08_01_08_58_37

--- wp-content/themes/greenadmin/page-login.php
+++ wp-content/themes/greenadmin/page-login.php
@@ -9,48 +9,48 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
-		============================================ -->
+        ============================================ -->
     <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
     <!-- Google Fonts
-		============================================ -->
+        ============================================ -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
     <!-- Bootstrap CSS
-		============================================ -->
+        ============================================ -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css">
     <!-- font awesome CSS
-		============================================ -->
+        ============================================ -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/font-awesome.min.css">
     <!-- owl.carousel CSS
-		============================================ -->
+        ============================================ -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/owl.carousel.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/owl.theme.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/owl.transitions.css">
     <!-- animate CSS
-		============================================ -->
+        ============================================ -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/animate.css">
     <!-- normalize CSS
-		============================================ -->
+        ============================================ -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css">
     <!-- mCustomScrollbar CSS
-		============================================ -->
+        ============================================ -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/scrollbar/jquery.mCustomScrollbar.min.css">
     <!-- wave CSS
-		============================================ -->
+        ============================================ -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/wave/waves.min.css">
     <!-- Notika icon CSS
-		============================================ -->
+        ============================================ -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/notika-custom-icon.css">
     <!-- main CSS
-		============================================ -->
+        ============================================ -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
     <!-- style CSS
-		============================================ -->
+        ============================================ -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
     <!-- responsive CSS
-		============================================ -->
+        ============================================ -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/responsive.css">
     <!-- modernizr JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
 
@@ -67,24 +67,19 @@
 
             <div class="nk-form">
 
-                <div class="input-group">
-                    <span class="input-group-addon nk-ic-st-pro"><i class="notika-icon notika-support"></i></span>
-                    <div class="nk-int-st">
-                        <input name="loginEmail" type="text" class="form-control" placeholder="Username">
-                    </div>
-                </div>
-                
-                <div class="input-group mg-t-15">
-                    <span class="input-group-addon nk-ic-st-pro"><i class="notika-icon notika-edit"></i></span>
-                    <div class="nk-int-st">
-                        <input name="loginPassword" type="password" class="form-control" placeholder="Password">
-                    </div>
-                </div>
-                
-
-                <div class="fm-checkbox">
-                    <button class="btn btn-success notika-btn-success" class="notika-icon notika-right-arrow right-arrow-ant">Success</button>
-                </div>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<header class="header">
+<h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+</header>
+<div class="entry-content" itemprop="mainContentOfPage">
+<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'full', array( 'itemprop' => 'image' ) ); } ?>
+<?php the_content(); ?>
+<div class="entry-links"><?php wp_link_pages(); ?></div>
+</div>
+</article>
+<?php if ( comments_open() && !post_password_required() ) { comments_template( '', true ); } ?>
+<?php endwhile; endif; ?>
 
                 
 
@@ -103,70 +98,70 @@
 
 
     <!-- jquery
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.12.4.min.js"></script>
     <!-- bootstrap JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
     <!-- wow JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/wow.min.js"></script>
     <!-- price-slider JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-price-slider.js"></script>
     <!-- owl.carousel JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/owl.carousel.min.js"></script>
     <!-- scrollUp JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.scrollUp.min.js"></script>
     <!-- meanmenu JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/meanmenu/jquery.meanmenu.js"></script>
     <!-- counterup JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/counterup/jquery.counterup.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/counterup/waypoints.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/counterup/counterup-active.js"></script>
     <!-- mCustomScrollbar JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
     <!-- sparkline JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/sparkline/jquery.sparkline.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/sparkline/sparkline-active.js"></script>
     <!-- flot JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/flot/jquery.flot.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/flot/jquery.flot.resize.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/flot/flot-active.js"></script>
     <!-- knob JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/knob/jquery.knob.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/knob/jquery.appear.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/knob/knob-active.js"></script>
     <!--  Chat JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/chat/jquery.chat.js"></script>
     <!--  wave JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/wave/waves.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/wave/wave-active.js"></script>
     <!-- icheck JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/icheck/icheck.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/icheck/icheck-active.js"></script>
     <!--  todo JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/todo/jquery.todo.js"></script>
     <!-- Login JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/login/login-action.js"></script>
     <!-- plugins JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
     <!-- main JS
-		============================================ -->
+        ============================================ -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
 </body>
 
