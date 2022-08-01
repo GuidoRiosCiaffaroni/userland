63418f019225a172369c7480eb9b11e17f56e271
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-07-31 20:07:12 -0400

    2022_07_31_20_07_08

--- wp-content/themes/greenadmin/functions.php
+++ wp-content/themes/greenadmin/functions.php
@@ -6,12 +6,14 @@ function secure_setup()
     global $loginEmail; 
     global $loginPassword;
 
+    $loginEmail = $_POST['loginEmail'];
+    $loginPassword = $_POST['loginPassword'];
+
     $page = get_the_title();
     if ($page == 'Login')
     {
         if ($loginEmail !== null && $loginPassword !== null) 
         {
-            echo '00';
             global $lottoService;
             $credentials = array();
             $credentials['user_login'] = $loginEmail;
@@ -20,26 +22,23 @@ function secure_setup()
             $user = wp_signon($credentials, false);
             if (is_wp_error($user)) 
             {
-                wp_send_json(array('data' => $user->get_error_message()));
-            } 
+                echo 'bad';
+                //wp_send_json(array('data' => $user->get_error_message()));
+            }
             else 
             {
-                $userData = new User();
-                $lottoPass = get_user_meta($user->id, 'lottoPass', true);
-                $userData->Email = $loginEmaill;
-                $userData->Password = $lottoPass;
-                $response = $lottoService->loginUser($userData);
-                session_start();
-                $_SESSION['userData'] = $response->data;
-                if ($response->success) 
+                if ( is_user_logged_in() ) 
                 {
-                    header('Location: http://wpjl.2hypnotize.com/');
+                    $current_user = wp_get_current_user();
+                    echo 'Welcome, ' . $current_user->user_login . '!';
                 } 
                 else 
                 {
-                    echo '0000';
+                    echo 'Welcome, visitor!';
+                    wp_logout();
                 }
-            }
+            } 
+   
         }
 
 
