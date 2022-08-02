ead6edff5c2173c4bd4dba19d3dcb134c1bf2a69
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-08-02 16:34:50 -0400

    2022_08_02_16_34_50

--- wp-content/themes/greenadmin/page-perfil.php
+++ wp-content/themes/greenadmin/page-perfil.php
@@ -13,67 +13,14 @@
 										<i class="notika-icon notika-edit"></i>
 									</div>
 									<div class="breadcomb-ctn">
-									   
-
-									
-									
-									<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-									<header class="header">
-									<h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-									</header>
-									<div class="entry-content" itemprop="mainContentOfPage">
-
-									<?php
-									    $current_user = wp_get_current_user();
-  										$roles = ( array ) $current_user->roles;
-
-									    //echo "User Login :".$current_user->user_login;
-									    //echo '</br>';
-									    //echo "User ID :".$current_user->ID;
-									    //echo '</br>';
-									    //echo "User Pass :".$current_user->user_pass;
-									    //echo '</br>';
-									    //echo "User NicName :".$current_user->user_nicename;
-									    //echo '</br>';
-									    //echo "User Email :".$current_user->user_email;
-									    //echo '</br>';
-									    //echo "User URL :".$current_user->user_url;
-									    //echo '</br>';
-									    //echo "User Registered :".$current_user->user_registered;
-									    //echo '</br>';
-									    //echo "User Activation Key :".$current_user->user_activation_key;
-									    //echo '</br>';
-									    //echo "User Status :".$current_user->user_status;
-									    //echo '</br>';
-									    //echo "User Display Name :".$current_user->display_name;   
-										//echo '</br>';
-									    //echo 'User level: ' . $current_user->user_level;
-  										//echo '</br>';
-  										//echo 'User first name: ' . $current_user->user_firstname;
-  										//echo '</br>';
-  										//echo 'User last name: ' . $current_user->user_lastname;
-  										//echo '</br>';
-  										//echo 'User Roles: ' . $current_user->roles;
-										//echo '</br>';
-  										//echo 'User Roles: '.$roles[0];
-  										//echo '</br>';
-  										//echo 'User avatar por defecto : ' . get_avatar( $current_user->ID, 64 ) . '<br />';
-										//echo '</br>';
-  										//echo 'User avatar por original : ' . get_wp_user_avatar($current_user->ID, 'original');
-  										//echo '</br>';
-  										//echo 'User avatar por large : ' . get_wp_user_avatar($current_user->ID, 'large');
-  										//echo '</br>';
-  										//echo 'User avatar por medium : ' . get_wp_user_avatar($current_user->ID, 'medium');
-  										//echo '</br>';
-  										//echo 'User avatar por thumbnail : ' . get_wp_user_avatar($current_user->ID, 'thumbnail');
-									?>
-																		
-									<div class="entry-links"><?php wp_link_pages(); ?></div>
-									</div>
-									</article>
-									
-
-
+										<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+											<header class="header">
+												<h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+											</header>
+											<div class="entry-content" itemprop="mainContentOfPage">	
+												<div class="entry-links"><?php wp_link_pages(); ?></div>
+											</div>
+										</article>
                                     </div>    
 								</div>
 							</div>
@@ -89,7 +36,11 @@
 
 
 
-<div class="row">
+			<div class="row">
+			<?php
+				$current_user = wp_get_current_user();
+  				$roles = ( array ) $current_user->roles;
+			?>	
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
                     <div class="normal-table-list">
