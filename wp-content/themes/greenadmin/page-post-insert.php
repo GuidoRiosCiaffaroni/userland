75533c6c14c8a3a0f0853a7756f4ed2abe42d596
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-08-07 20:40:56 -0400

    2022_08_07_20_40_54

--- wp-content/themes/greenadmin/page-post-insert.php
+++ wp-content/themes/greenadmin/page-post-insert.php
@@ -9,7 +9,7 @@
                     <div class="form-element-list">
                         <div class="basic-tb-hd">
                             <h2>Texto Para Ingresar</h2>
-                            <p>Text.</p>
+                            <!-- <p>Text.</p> -->
                         </div>
 
                         <div class="row">
@@ -25,7 +25,7 @@
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                 <div class="cmp-tb-hd">
                                     <h2>HTML Editor</h2>
-                                    <p>Super Simple WYSIWYG Editor on Bootstrap</p>
+                                    <!-- <p>Super Simple WYSIWYG Editor on Bootstrap</p> -->
                                 </div>
                                     
                                 <div class="cmp-tb-hd bsc-smp-sm">
@@ -35,22 +35,13 @@
                                 <div class="html-editor">
                                 </div>
                                     
-                                <div class="cmp-tb-hd mg-t-20">
-                                    <h2>Click to edit</h2>
-                                    <p>You can edit content on the fly</p>
-                                </div>
-                                    
-                                <div class="summernote-clickable">
-                                    <button class="btn btn-primary btn-sm hec-button">Click here to edit the content</button>
-                                    <button class="btn btn-success btn-sm hec-save" style="display:none;">Save</button>
-                                </div>
+
                             </div>
                    
-                
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                 <div class="cmp-tb-hd">
                                     <h2>Idioma de Origen</h2>
-                                    <p>Super Simple WYSIWYG Editor on Bootstrap</p>
+                                    <!-- <p>Super Simple WYSIWYG Editor on Bootstrap</p> -->
                                 </div>
                                     
                                 <div class="bootstrap-select fm-cmp-mg">
@@ -62,14 +53,39 @@
                                     
                             </div>
 
-                            
+ 
+
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <div class="cmp-tb-hd">
+                                    <h2>Idioma de Origen</h2>
+                                    <!-- <p>Super Simple WYSIWYG Editor on Bootstrap</p> -->
+                                </div>
+                                    
+                            <div class="fm-checkbox">
+                                <button class="btn btn-success notika-btn-success" class="notika-icon notika-right-arrow right-arrow-ant">Ingresar</button>
+                            </div>
+
+
+
+                                <div class="cmp-tb-hd mg-t-20">
+                                    <h2>Click to edit</h2>
+                                    <!-- <p>You can edit content on the fly</p>-->
+                                </div>
+                                    
+                                <div class="summernote-clickable">
+                                    <button class="btn btn-primary btn-sm hec-button">Click here to edit the content</button>
+                                    <button class="btn btn-success btn-sm hec-save" style="display:none;">Save</button>
+                                </div>
+
+
+                                    
+                            </div>
+
 
             
 
 
-                            <div class="fm-checkbox">
-                                <button class="btn btn-success notika-btn-success" class="notika-icon notika-right-arrow right-arrow-ant">Ingresar</button>
-                            </div>
+
                             
                         </div>
 
