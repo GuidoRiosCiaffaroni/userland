77b159c6c78a3fc17527ef7e28d5c209f4376384
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-08-07 20:52:08 -0400

    2022_08_07_20_52_03

--- wp-content/themes/greenadmin/page-post-insert.php
+++ wp-content/themes/greenadmin/page-post-insert.php
@@ -56,46 +56,26 @@
  
 
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <!--
                                 <div class="cmp-tb-hd">
                                     <h2>Idioma de Origen</h2>
-                                    <!-- <p>Super Simple WYSIWYG Editor on Bootstrap</p> -->
+                                    <p>Super Simple WYSIWYG Editor on Bootstrap</p>
                                 </div>
+                                -->
                                     
-                            <div class="fm-checkbox">
-                                <button class="btn btn-success notika-btn-success" class="notika-icon notika-right-arrow right-arrow-ant">Ingresar</button>
-                            </div>
-
-
-
-                                <div class="cmp-tb-hd mg-t-20">
-                                    <h2>Click to edit</h2>
-                                    <!-- <p>You can edit content on the fly</p>-->
+                                <div class="fm-checkbox">
+                                    <button class="btn btn-success notika-btn-success" class="notika-icon notika-right-arrow right-arrow-ant">Ingresar</button>
                                 </div>
                                     
                                 <div class="summernote-clickable">
                                     <button class="btn btn-primary btn-sm hec-button">Click here to edit the content</button>
                                     <button class="btn btn-success btn-sm hec-save" style="display:none;">Save</button>
-                                </div>
-
-
-                                    
+                                </div>    
                             </div>
-
-
-            
-
-
-
                             
                         </div>
 
-                         <div class="row">
-                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                <div class="floating-numner fm-ele-mg">
-                                    <p> Ingrese al Frase o Palabra .</p>
-                                </div>
-                            </div>
-                        </div>
+
 
                         <!--
                         <?php echo get_the_category_list(); ?>
