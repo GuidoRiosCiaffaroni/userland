4d540ac484298fa57a126793fac329f39d36b909
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-08-21 00:39:33 -0400

    2022_08_21_00_39_31

--- wp-content/themes/greenadmin/page-post-details.php
+++ wp-content/themes/greenadmin/page-post-details.php
@@ -39,6 +39,148 @@
                             <h2>Basic Example</h2>
                             <p>It's just that simple. Turn your simple table into a sophisticated data table and offer your users a nice experience and great features without any effort.</p>
                         </div>
+
+                        <?php
+                            $post = get_post( $_GET['ID'] ); 
+
+                            
+
+
+                        ?>
+
+                        <div class="table-responsive">
+                            <table id="data-table-basic" class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Detalle</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                <tr>   
+                                    <td>ID :</td>
+                                    <td><?php echo $post->ID; ?></td>
+                                </tr>
+                                
+                                <tr>   
+                                    <td>post author ID :</td>
+                                    <td><?php echo $post->post_author; ?></td>
+                                </tr>
+
+                                <tr>   
+                                    <td>post name :</td>
+                                    <td><?php echo $post->post_name; ?></td>
+                                </tr>
+
+                                <tr>   
+                                    <td>post type :</td>
+                                    <td><?php echo $post->post_type; ?></td>
+                                </tr>
+
+                                <tr>   
+                                    <td>post title :</td>
+                                    <td><?php echo $post->post_title; ?></td>
+                                </tr>
+
+                                <tr>   
+                                    <td>post date :</td>
+                                    <td><?php echo $post->post_date; ?></td>
+                                </tr>
+
+                                <tr>   
+                                    <td>post date gmt :</td>
+                                    <td><?php echo $post->post_date_gmt; ?></td>
+                                </tr>
+
+                                <tr>   
+                                    <td>post content :</td>
+                                    <td><?php echo $post->post_content; ?></td>
+                                </tr>  
+
+                                <tr>   
+                                    <td>post excerpt :</td>
+                                    <td><?php echo $post->post_excerpt; ?></td>
+                                </tr>                                  
+
+                                <tr>   
+                                    <td>post statust :</td>
+                                    <td><?php echo $post->post_status; ?></td>
+                                </tr>
+
+                                <tr>   
+                                    <td>comment status :</td>
+                                    <td><?php echo $post->comment_status; ?></td>
+                                </tr> 
+
+                                <tr>   
+                                    <td>ping status :</td>
+                                    <td><?php echo $post->ping_status; ?></td>
+                                </tr>                                                                                                                                
+                                <tr>   
+                                    <td>post passwords :</td>
+                                    <td><?php echo $post->post_password; ?></td>
+                                </tr>
+
+                                <tr>   
+                                    <td>post parent :</td>
+                                    <td><?php echo $post->post_parent; ?></td>
+                                </tr>
+
+                                <tr>   
+                                    <td>post modified :</td>
+                                    <td><?php echo $post->post_modified; ?></td>
+                                </tr>
+
+                                <tr>   
+                                    <td>post_modified_gmt :</td>
+                                    <td><?php echo $post->post_modified_gmt; ?></td>
+                                </tr>
+
+                                <tr>   
+                                    <td>comment_count :</td>
+                                    <td><?php echo $post->comment_count; ?></td>
+                                </tr>      
+                                
+                                <tr>   
+                                    <td>menu_order :</td>
+                                    <td><?php echo $post->menu_order; ?></td>
+                                </tr>  
+
+                                <tr>   
+                                    <td>meta _translation:</td>
+                                    <td><?php echo get_post_meta( $_GET['ID'], '_translation', true  ); ?></td>
+                                </tr> 
+
+                                <tr>   
+                                    <td>meta _ideogram:</td>
+                                    <td><?php echo get_post_meta( $_GET['ID'], '_ideogram', true  ); ?></td>
+                                </tr>
+
+                                <tr>   
+                                    <td>meta _pronunciation:</td>
+                                    <td><?php echo get_post_meta( $_GET['ID'], '_pronunciation', true  ); ?></td>
+                                </tr>     
+                                
+                                <tr>   
+                                    <td>meta _source_language:</td>
+                                    <td><?php echo get_post_meta( $_GET['ID'], '_source_language', true  ); ?></td>
+                                </tr>  
+                                
+                                <tr>   
+                                    <td>meta _target_language:</td>
+                                    <td><?php echo get_post_meta( $_GET['ID'], '_target_language', true  ); ?></td>
+                                </tr>                                                                                           
+
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th>Texto</th>
+                                        <th>Detalle</th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
